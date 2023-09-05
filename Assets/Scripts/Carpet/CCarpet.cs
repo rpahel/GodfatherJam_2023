5208183b5b2ee25d75b97619eea7691c5b161f54
Venizio <128746877+Venizio@users.noreply.github.com>
2023-09-05 16:43:59 +0200

    Update CCarpet.cs

--- Assets/Scripts/Carpet/CCarpet.cs
+++ Assets/Scripts/Carpet/CCarpet.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class CCarpet : MonoBehaviour
 {
+    Coroutine verifieur;
+    float seconds = 20f;
     private float m_speed = 10.0f;
     private bool m_senseOfRotation = true; // true = right
     // Start is called before the first frame update
     void Start()
     {
-
+        verifieur = StartCoroutine(RotationCooldown());
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (verifieur == null)
+        {
+            verifieur = StartCoroutine(RotationCooldown());
+        }
 
     }
 
@@ -25,7 +31,17 @@ public class CCarpet : MonoBehaviour
     public void ChangeRotation()
     {
         m_senseOfRotation = !m_senseOfRotation;
-
     }
+    private IEnumerator RotationCooldown()
+    {
 
+        yield return new WaitForSecondsRealtime(seconds);
+        ChangeRotation();
+        seconds = accelerateRotationChanging(seconds);
+        verifieur = null;
+    }
+    public float accelerateRotationChanging(float repeatTime)
+    {
+        return repeatTime / 1.4f;
+    }
 }
