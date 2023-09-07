1a1d2798ada4d469dbfd23df8e62607f2dd16891
Venizio <gabinpincon@gmail.com>
2023-09-07 16:06:39 +0200

    update

--- Assets/Scripts/Conveyor Belt/CConveyorBelt.cs
+++ Assets/Scripts/Conveyor Belt/CConveyorBelt.cs
@@ -5,83 +5,110 @@ using UnityEngine;
 public class CConveyorBelt : MonoBehaviour
 {
     CPlayerMovements player;
-    Vector2 vectRight;
-    Vector2 vectLeft;
+    Rigidbody2D playerRb;
+    float vectRight;
+    float vectLeft;
     Vector2 vectInitial;
     bool isEnter = false;
     private float m_speed = 1.5f;
     public bool m_senseOfRotation = true; // true = right
     float playerSpeed;
     float playerSpeedInverse;
+    public float DebutRotation;
+    public float timeRepeatIteration;
+    public float IncreasingSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("ChangeRotation", 20.0f, 5.0f);
-        vectInitial = player.Rbobj.velocity;
-        vectLeft = player.Rbobj.velocity;
-        vectRight = player.Rbobj.velocity;
+        InvokeRepeating("ChangeRotation", DebutRotation, timeRepeatIteration);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         IncreaseSpeed();
+
+        if (!player)
+            return;
+
         playerSpeed = vectInitial.x + m_speed;
         playerSpeedInverse = vectInitial.x - m_speed;
-
         if (isEnter == true && m_senseOfRotation == true)
         {
-            player.Rbobj.velocity = vectRight;
+            player.Rbobj.velocity = new Vector2(vectRight, player.Rbobj.velocity.y * 1.4f);
         }
         if (isEnter == true && m_senseOfRotation == false)
         {
-            player.Rbobj.velocity = vectLeft;
+            player.Rbobj.velocity = new Vector2(vectLeft, player.Rbobj.velocity.y * 1.4f);
         }
-        Debug.Log(m_speed);
 
     }
-    void OnCollisionEnter2D(Collision2D col)
+    private void OnCollisionEnter2D(Collision2D col)
     {
+
         if (col.gameObject.TryGetComponent(out player))
         {
+            
+           // Debug.Log($"1 : {player.name}");
+            playerRb = col.gameObject.GetComponent<Rigidbody2D>();
+            vectInitial = playerRb.velocity;
+            vectLeft = player.Rbobj.velocity.x;
+            vectRight = player.Rbobj.velocity.x;
             InitialisePlayerVelocity();
             isEnter = true;
         }
-    }
-    private void OnCollisionStay2D(Collision2D stay)
-    {
-        if (stay.gameObject.TryGetComponent(out player))
+        else if(col.transform.parent.TryGetComponent(out player))
         {
+            
+            //Debug.Log($"2 : {player.name}");
+            playerRb = col.gameObject.GetComponent<Rigidbody2D>();
+            vectInitial = playerRb.velocity;
+            vectLeft = player.Rbobj.velocity.x;
+            vectRight = player.Rbobj.velocity.x;
             InitialisePlayerVelocity();
+            isEnter = true;
         }
+        
+
     }
     bool InitialisePlayerVelocity()
     {
-        vectLeft = new Vector2(playerSpeedInverse, 0);
-        vectRight = new Vector2(playerSpeed, 0);
+        vectLeft = playerSpeedInverse;
+        vectRight = playerSpeed;
         return true;
     }
 
     void OnCollisionExit2D(Collision2D collision)
     {
+
         if (collision.gameObject.TryGetComponent(out player))
         {
             player.Rbobj.velocity = vectInitial;
             isEnter = false;
+            player = null;
         }
+        //else if (collision.transform.parent.TryGetComponent(out player))
+        //{
+        //    player.Rbobj.velocity = vectInitial;
+        //    isEnter = false;
+        //    player = null;
+        //
+        //
+        //}
+        
     }
 
 
     public void ChangeRotation()
     {
         m_senseOfRotation = !m_senseOfRotation;
-        Debug.Log("sens changer");
+        //Debug.Log("sens changer");
     }
 
     void IncreaseSpeed()
     {
-        m_speed = m_speed + Time.deltaTime/8;
+        m_speed += Time.deltaTime * IncreasingSpeed / 100;
     }
 
 }
