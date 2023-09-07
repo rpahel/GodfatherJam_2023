892998d0fbd94dec15974fc3dc257bec320e1176
LordBrach <alessandrotano@outlook.fr>
2023-09-07 15:26:51 +0200

    Updated player movement and interaction with obstacles

--- Assets/Scripts/PlayerCharacter/Movements/CPlayerMovements.cs
+++ Assets/Scripts/PlayerCharacter/Movements/CPlayerMovements.cs
@@ -57,7 +57,7 @@ public class CPlayerMovements : MonoBehaviour
                     radius: circleCollider.radius * 0.5f,
                     direction: Vector2.down,
                     distance: circleCollider.radius * 0.5f + 0.1f,
-                    layerMask: LayerMask.GetMask("Belt")))
+                    layerMask: LayerMask.GetMask("Belt", "Obstacle")))
             return true;
 
         return false;
